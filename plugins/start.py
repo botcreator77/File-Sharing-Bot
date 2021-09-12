769a264eda09091f5a2f88662e66f8c495d3b5c8
botcreator77 <89454292+botcreator77@users.noreply.github.com>
2021-09-12 14:22:24 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -73,8 +73,8 @@ async def start_command(client: Client, message: Message):
         reply_markup = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    InlineKeyboardButton("MOVIE SEARCH HERE", url = "https://t.me/malayayalies"),
+                    InlineKeyboardButton("NEW MOVIES (2021)", url = "https://t.me/joinchat/FClPZ54OrtdlNjQ1")
                 ]
             ]
         )
